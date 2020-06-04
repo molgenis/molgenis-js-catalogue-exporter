b73a39499fa96c3e259f3aef22c233fab91b20c1
Fleur Kelpin <f.kelpin@umcg.nl>
2020-06-04 21:53:26 +0200

    style: lint the files

--- test/fetch.test.ts
+++ test/fetch.test.ts
@@ -25,26 +25,30 @@ beforeEach(jest.resetAllMocks)
 
 describe('fetchData', () => {
   it('returns fetched variables', async done => {
-    (fetch as any).mockResolvedValue({json: jsonMock})
-    jsonMock.mockResolvedValue({ items: [variable1]})
+    ;(fetch as any).mockResolvedValue({ json: jsonMock })
+    jsonMock.mockResolvedValue({ items: [variable1] })
     const variables = await fetchData('https://example.org/api/v2/Variables')
     expect(variables).toEqual([variable1])
     done()
   })
 
   it('fetches multiple pages', async done => {
-    (fetch as any).mockResolvedValue({json: jsonMock})
-    jsonMock.mockResolvedValueOnce({
-      items: [variable1], 
-      nextHref: 'https://example.org/api/v2/Variables?start=20'
-    }).mockResolvedValueOnce({
-      items: [variable2]
-    })
+    ;(fetch as any).mockResolvedValue({ json: jsonMock })
+    jsonMock
+      .mockResolvedValueOnce({
+        items: [variable1],
+        nextHref: 'https://example.org/api/v2/Variables?start=20'
+      })
+      .mockResolvedValueOnce({
+        items: [variable2]
+      })
     const variables = await fetchData('https://example.org/api/v2/Variables')
     expect(variables).toEqual([variable1, variable2])
     expect(fetch).toHaveBeenCalledTimes(2)
     expect(fetch).toHaveBeenCalledWith('https://example.org/api/v2/Variables')
-    expect(fetch).toHaveBeenCalledWith('https://example.org/api/v2/Variables?start=20')
+    expect(fetch).toHaveBeenCalledWith(
+      'https://example.org/api/v2/Variables?start=20'
+    )
     done()
   })
 })
