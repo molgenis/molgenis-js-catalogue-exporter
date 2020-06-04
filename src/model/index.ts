55838c1739c1e964f7b1844afe8385b824ceff18
Fleur Kelpin <f.kelpin@umcg.nl>
2020-06-04 13:24:23 +0200

    refactor: make testable

---
+++ src/model/index.ts
@@ -0,0 +1,50 @@
+export interface Dictionary<T> {
+  [index: string]: T
+}
+
+export interface Emx {
+  attributes: Attribute[]
+  data: Dictionary<Option[]>
+}
+
+export enum CatalogueDatatype {
+  CATEGORICAL = 'categorical',
+  INTEGER = 'int',
+  BINARY = 'binary',
+  CONTINUOUS = 'continuous'
+}
+
+export interface Option {
+  id: string
+  label: string
+}
+
+export interface Variable {
+  variable: string
+  label: string
+  datatype: {
+    id: CatalogueDatatype
+  }
+  values?: string
+}
+
+export enum MolgenisDataType {
+  STRING = 'string',
+  INTEGER = 'int',
+  CATEGORICAL = 'categorical',
+  BOOLEAN = 'bool',
+  DECIMAL = 'decimal'
+}
+
+export interface Attribute {
+  name: string
+  label?: string
+  description?: string
+  dataType: MolgenisDataType
+  entity: string
+  idAttribute?: 'TRUE' | 'FALSE' | 'AUTO'
+  labelAttribute?: 'TRUE' | 'FALSE'
+  rangeMin?: number
+  rangeMax?: number
+  refEntity?: string
+}
---
+++ src/zip/index.ts
@@ -0,0 +1,55 @@
+import JSZip from 'jszip'
+import stringify from 'csv-stringify'
+import type { Emx } from 'model'
+
+export const asZip = async (emx: Emx): Promise<JSZip> => {
+  const zip: JSZip = new JSZip()
+  await addCsv(
+    zip,
+    [
+      'entity',
+      'name',
+      'description',
+      'dataType',
+      'labelAttribute',
+      'idAttribute',
+      'refEntity',
+      'rangeMin',
+      'rangeMax'
+    ],
+    'attributes.csv',
+    emx.attributes
+  )
+  await Promise.all(
+    Object.entries(emx.data).map(([sheet, options]) =>
+      addCsv(zip, ['id', 'label'], `${sheet}.csv`, options)
+    )
+  )
+  return zip
+}
+
+const addCsv = async (
+  zip: JSZip,
+  columns: string[],
+  filename: string,
+  data: object[]
+) => {
+  return new Promise((resolve, reject) => {
+    stringify(
+      data,
+      {
+        header: true,
+        columns
+      },
+      function (err, data) {
+        if (err) {
+          reject(err)
+        }
+        if (data) {
+          zip.file(filename, data)
+          resolve()
+        }
+      }
+    )
+  })
+}
